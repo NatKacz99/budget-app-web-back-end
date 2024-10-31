db73f3501dfa53cecb25729fcfaa4f6ec1f622f6
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2024-10-31 14:34:28 +0100

    added starting flies

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("log-in").addEventListener("click", function () {
+  window.open("strona_logowania.html");
+});
+
+document.getElementById("sign-up").addEventListener("click", function () {
+  window.open("strona_rejestracji.html");
+});
+
